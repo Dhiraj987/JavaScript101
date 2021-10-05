c584b325cd079108e03fbb7494620f8ba55fb557
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-10-05 00:34:22 -0400

    variables and concatination

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+// concatination
+let name = 'Sam';
+console.log("Hello " + name);
+
+// declaring variables
+let firstName = 'Sam'; 
+let lastName = "Ludwang"
+console.log(firstName + ' ' + lastName)
