e0ecb23b4ff85e88771361606efff38bb512b634
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-10-05 00:40:31 -0400

    primitive data types

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ let firstName = 'Sam';
 let lastName = "Ludwang";
 console.log(firstName + ' ' + lastName);
 
-
 //constants
 const birthDate = '1994-01-04';
 console.log(birthDate);
+
+// primitive types-- these are the prim data types in JS
+let name = 'Sam';           //String Literal
+let age = 30;               //Number Literal
+let isApproved = false      // Boolean Literal
+let address = null;         // Null Literal
+let iDontKnow = undefined   // Undefined Literal
