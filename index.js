f13d8fd4a0ec1b5b2f01eed4b1fcea06ed7c4a2a
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-10-05 00:53:27 -0400

    objects in js

--- index.js
+++ index.js
@@ -14,8 +14,34 @@ const birthDate = '1994-01-04';
 console.log(birthDate);
 
 // primitive types-- these are the prim data types in JS
-let name = 'Sam';           //String Literal
+let name_a = 'Sam';          //String Literal
 let age = 30;               //Number Literal
 let isApproved = false      // Boolean Literal
-let address = null;         // Null Literal
+let address = null;         // Object or null
 let iDontKnow = undefined   // Undefined Literal
+
+//Dynamic Programming Language allows us to change the data
+// types of the variables meaning (say) age above can be changed
+// to a String from Int -- we can use the chrome console to verify this
+
+age = 'Sam or Wam; its a string now';
+
+//------------------------------------------------------------------------
+
+// Objects in JS
+
+// let name_for_here = 'Sam';
+// let age_for_here = 30
+// these could be converted into an object by the following ways
+
+let person = {
+    name_for_here: 'Sam',
+    age_for_here: 30
+};
+
+console.log(person.age_for_here);
+
+console.log(person.name_for_here);
+person.name_for_here = "Sammy";
+
+console.log(person.name_for_here);
