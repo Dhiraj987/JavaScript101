2cb29cd9603466a2e1dd6147bfe7e1894f75bb08
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-10-05 00:36:16 -0400

    constants

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ console.log("Hello " + name);
 
 // declaring variables
 let firstName = 'Sam'; 
-let lastName = "Ludwang"
-console.log(firstName + ' ' + lastName)
+let lastName = "Ludwang";
+console.log(firstName + ' ' + lastName);
+
+
+//constants
+const birthDate = '1994-01-04';
+console.log(birthDate);
