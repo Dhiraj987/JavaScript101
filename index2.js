957f722675d6b8c2f99b0f6b2017656b03de9aa4
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-10-05 15:05:55 -0400

    operators

---
+++ index2.js
@@ -0,0 +1,29 @@
+// continued from the first file
+
+
+// types of operators in JS -- Arithmetic, Assignment, Comparison, Logical, Bitwise
+
+
+// Arithmetic operators
+let x = 10;
+let y = 5;
+
+console.log(x*y);
+console.log(x/y);
+console.log(x+y);
+console.log(x-y);
+console.log(x**y)
+console.log(x%y)
+
+
+console.log('---------------------------')
+
+// increment
+// increase the value first and then display it
+console.log(++x)
+
+console.log(x++)
+console.log(x)
+// decrement
+
+console.log(--x)
